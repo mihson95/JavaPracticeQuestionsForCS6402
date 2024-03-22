ee0ea46dba31c75e1a3dc19574ba8a714fc7fff3
mihson95 <sontakemihir@gmail.com>
2024-03-22 11:16:12 +0000

    Second Problem

---
+++ Week 8/Problem2.md
@@ -0,0 +1,7 @@
+## Write a Program in Java to Add two Numbers
+
+### Programming Constructs Used in This Program
+
+- Scanner = Get Entry from User. Need to import from java.util.
+
+- Operators = Mathematical Operators are used to sum integer values
---
+++ Week 8/SolutionForProblem2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class SolutionForProblem2 {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Get First Number as User Input");
+        int a = sc.nextInt();
+        System.out.println("Get Second Number as User Input");
+        int b = sc.nextInt();
+        int sum = a + b;
+
+        System.out.println("Sum of 2 numbers = " + sum);
+        sc.close();
+    }
+    
+}
