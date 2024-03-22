e6505558c6655a37d9ef890baf8e57f50bb0384e
mihson95 <sontakemihir@gmail.com>
2024-03-22 11:03:07 +0000

    Problem 1 Solution

---
+++ Week 8/Problem1.md
@@ -0,0 +1,7 @@
+## Write Hello World Program in Java
+
+### Programming Constructs Used in This Program
+
+- main function = entry part of program
+
+- Println Function = Print the Function with a new line at the end
---
+++ Week 8/SolutionForProblem1.java
@@ -0,0 +1,10 @@
+// Entry Level Java Program to Print Hello World
+
+// Driver Class
+public class SolutionForProblem1 {
+    // main function
+    public static void main(String[] args) {
+        // Printing Hello World
+        System.out.println("Hello World!");
+    }
+}
